76afe3cc3c8a754ffdc3fe02c4698402de02eeeb
Anna Marín <annamarinnunyo@gmail..com>
2026-06-05 15:22:36 +0200

    1

--- sort.c
+++ sort.c
@@ -1,11 +1,30 @@
 #include <stdint.h>
 
-#define N 8
+#define N 12
+
+
+void swap(int8_t *arr, int i, int j);
 
 void sort (int8_t a[], int size) {
-	// write your favorite sorting algorithm
+	 for (int i = 0; i < size - 1; i++)
+    {
 
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            if (a[j] > a[j + 1])
+                swap(a, j, j + 1);
+        }
+    }
 }
+
+
+void swap(int8_t *arr, int i, int j)
+{
+    int8_t temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
 
 void checkSort() {
 
@@ -19,5 +38,13 @@ void checkSort() {
 	sort (array, N);
 
 	// write the assertions to check that the array is sorted
+	for (i = 0; i < N - 1; ++i){
+		__CPROVER_assert(array[i] <= array[i + 1], "Array is not sorted");
+	}
 
+}
+
+int main() {
+	checkSort();
+	return 0;
 }
